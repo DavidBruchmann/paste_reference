028654200b3be1c3c254316d5528dabcab6c52df
David Bruchmann <david.bruchmann@gmail.com>
2026-07-16 16:05:19 +0700

    [FEATURE] rename & update included EXT:test_sitepacke
    * rename
    * add CE with irre content for testing

--- Tests/Extensions/test_container/ext_localconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('TYPO3') or die();
-
-// Register container elements for testing
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('container')) {
-    // Container registration is done in TCA/Overrides/tt_content.php
-}
